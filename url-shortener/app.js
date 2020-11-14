1bc9407bef400bd1bc7f58bc33a17db6f5e3386a
emmanuel adrian <e.adrian@myxtramile.com>
2020-11-14 13:52:42 +0100

    add linter / prettier and pre-commit hooks

--- url-shortener/app.js
+++ url-shortener/app.js
@@ -1,44 +1,20 @@
-const redis = require('redis')
-const redisClient = redis.createClient()
-const express = require('express')
-const app = express()
+'use strict';
+import redis from 'redis';
+import express from 'express';
+import { redirectHandler, shortenHandler } from './handlers.js';
 
-const port = process.env.PORT || 3000
+const app = express();
+const redisClient = redis.createClient();
+const port = process.env.PORT || 3000;
 
-app.get('/:id', async (req, res) => {
-  const id = req.params.id
-
-  if (!id) {
-    const statusCode = 406
-    res.status(statusCode).send({
-      message: 'identifier is missing from parameters',
-      'http-code': statusCode,
-    })
-  }
-
-  redisClient.get(id, (err, value) => {
-    if (err) {
-      throw err
-    }
-    res.redirect(value)
-  })
-})
+app.get('/:id', (req, res) => {
+    redirectHandler(req, res, redisClient);
+});
 
 app.post('/shorten', (req, res) => {
-  const urlToShorten = req.query.url
-  if (!urlToShorten) {
-    const statusCode = 406
-    res.status(statusCode).send({
-      message: 'url parameter is missing from query',
-      'http-code': statusCode,
-    })
-  }
-
-  const randomID = Math.random().toString(36).substring(7)
-  redisClient.set(randomID, urlToShorten, redis.print)
-  res.status(201).send({ url: `http://localhost:3000/${randomID}` })
-})
+    shortenHandler(req, res, redisClient);
+});
 
 app.listen(port, () => {
-  console.log(`server is listening on ${port}`)
-})
+    console.log(`server is listening on ${port}`);
+});
