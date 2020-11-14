6f29f40acc17cb28ecf5395753fc7d0e52d93a6c
emmanuel adrian <e.adrian@myxtramile.com>
2020-11-14 14:34:40 +0100

    add index.html to test

--- url-shortener/app.js
+++ url-shortener/app.js
@@ -1,9 +1,16 @@
 'use strict';
 import redis from 'redis';
+import cors from 'cors';
 import express from 'express';
 import { redirectHandler, shortenHandler } from './handlers.js';
 
 const app = express();
+
+const corsOptions = {
+    optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 const redisClient = redis.createClient();
 const port = process.env.PORT || 3000;
 
